61621dbeda5d2c6fbb4817b3401906fd09d9a2c3
Cristina S. Barreiro <cristina.sbarreiro@gmail.com>
2017-07-22 17:35:41 +0200

    Initial Commit

---
+++ iOS-Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS-Calculator
+//
+//  Created by cristina on 22/7/17.
+//  Copyright © 2017 crisbarreiro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
